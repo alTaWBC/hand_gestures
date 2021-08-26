ad4d2e08cbc69f194cdca57d777b88c42f0d0432
alTabWBC <wd.costa@campus.fct.unl.pt>
2021-08-26 20:06:19 +0100

    Improved Interface

--- mouse_example.py
+++ mouse_example.py
@@ -1,9 +1,36 @@
 import mouse
+import time
 
-print(mouse.get_position())
+events = []
 
-mouse.click('left')
-mouse.click('right')
-mouse.drag(0, 0, 100, 100, absolute=False, duration=0.1)
-events = mouse.record()
-mouse.play(events[:-1])
+
+def record_mouse():
+    mouse.hook(events.append)
+
+
+def stop_recording():
+    mouse.unhook(events.append)
+
+
+def click(key):
+    assert key in ['left', 'right', 'middle']
+    mouse.click(key)
+
+
+def move(right, down, duration=0.1, absolute=False):
+    mouse.move(right, down, absolute=absolute, duration=duration)
+
+
+def scroll(direction, amount):
+    assert direction in ['up', 'down']
+    scroll = [1, -1][['up', 'down'].index(direction)] * amount
+    mouse.wheel(scroll)
+
+
+def postion():
+    return mouse.get_position()
+
+
+def pressed(key):
+    assert key in ['left', 'right', 'middle']
+    return mouse.is_pressed(key)
