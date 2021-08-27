f024c537151ee827dc7c3aceaa3153f4c5ed69f5
alTabWBC <wd.costa@campus.fct.unl.pt>
2021-08-27 20:27:42 +0100

    First Attempt at recorder

--- emergency_button.py
+++ emergency_button.py
@@ -1,3 +1,4 @@
 #%%%
 import keyboard
 keyboard.release('shift')
+# %%
--- inputs.py
+++ inputs.py
@@ -1,17 +1,90 @@
 import pandas as pd
 from pynput import keyboard, mouse
+import time
+
+columns = ['Controller', 'Type', 'Button',
+           'Coordinates', 'ScrollDirection', 'Time']
+events = pd.DataFrame(columns=columns)
+
+mouse_controller = mouse.Controller()
+keyboard_controller = keyboard.Controller()
+
+position = mouse_controller.position
+
+
+def clear():
+    global events
+    events = pd.DataFrame(columns=columns).append({
+        'Controller': 'Mouse',
+        'Type': 'Initial',
+        'Button': 'None',
+        'Coordinates': mouse_controller.position,
+        'ScrollDirection': (0, 0),
+        'Time': time.time()
+    }, ignore_index=True)
 
 
 def mouse_click(x, y, button, pressed):
-    print(x, y, button, pressed)
+    global events, position
+    events = events.append({
+        'Controller': 'Mouse',
+        'Type': 'Press' if pressed else 'Release',
+        'Button': button,
+        'Coordinates': (x-position[0], y-position[1]),
+        'ScrollDirection': (0, 0),
+        'Time': time.time()
+    }, ignore_index=True)
+    position = (x, y)
 
 
 def mouse_move(x, y):
-    print(x, y)
+    global events, position
+    events = events.append({
+        'Controller': 'Mouse',
+        'Type': 'Move',
+        'Button': 'None',
+        'Coordinates': (x-position[0], y-position[1]),
+        'ScrollDirection': (0, 0),
+        'Time': time.time()
+    }, ignore_index=True)
+    position = (x, y)
 
 
 def mouse_scroll(x, y, dx, dy):
-    print(x, y, dx, dy)
+    global events, position
+    events = events.append({
+        'Controller': 'Mouse',
+        'Type': 'Scroll',
+        'Button': 'None',
+        'Coordinates': (x-position[0], y-position[1]),
+        'ScrollDirection': (dx, dy),
+        'Time': time.time()
+    }, ignore_index=True)
+    position = (x, y)
+
+
+def keyboard_press(key):
+    global events
+    events = events.append({
+        'Controller': 'Keyboard',
+        'Type': 'Press',
+        'Button': key,
+        'Coordinates': (0, 0),
+        'ScrollDirection': (0, 0),
+        'Time': time.time()
+    }, ignore_index=True)
+
+
+def keyboard_release(key):
+    global events
+    events = events.append({
+        'Controller': 'Keyboard',
+        'Type': 'Release',
+        'Button': key,
+        'Coordinates': (0, 0),
+        'ScrollDirection': (0, 0),
+        'Time': time.time()
+    }, ignore_index=True)
 
 
 mouse_listener = mouse.Listener(
@@ -21,23 +94,57 @@ mouse_listener = mouse.Listener(
 )
 
 
-def keyboard_press(key):
-    try:
-        key = key.char
-    except:
-        print('Cannot convert to char')
-    print('pressed', key)
-
-
-def keyboard_release(key):
-    try:
-        key = key.char
-    except:
-        print('Cannot convert to char')
-    print('released', key)
-
-
 keyboard_listener = keyboard.Listener(
     on_press=keyboard_press,
     on_release=keyboard_release
 )
+
+
+def start():
+    clear()
+    mouse_listener.start()
+    keyboard_listener.start()
+
+
+def stop():
+    mouse_listener.stop()
+    keyboard_listener.stop()
+
+
+def play():
+    previous = -1
+    for _, line in events.iterrows():
+        waitTime = line.Time - previous if previous != -1 else 0
+        previous = line.Time
+        event_controller = line.Controller
+        time.sleep(waitTime)
+        if event_controller == 'Mouse':
+            playMouse(line.Type, line.Button,
+                      line.Coordinates, line.ScrollDirection)
+        elif event_controller == 'Keyboard':
+            playKeyboard(line.Type, line.Button)
+
+
+def playMouse(types, button, coordinates, scroll):
+    if types == 'Initial':
+        mouse_controller.move(
+            coordinates[0] - mouse_controller.position[0], coordinates[1] - mouse_controller.position[0])
+    else:
+        mouse_controller.move(coordinates[0], coordinates[1])
+        if types == 'Press':
+            mouse_controller.press(button)
+        elif types == 'Release':
+            mouse_controller.release(button)
+        elif types == 'scroll':
+            mouse_controller.scroll(scroll[0], scroll[1])
+        else:
+            print(types, 'is not supported')
+
+
+def playKeyboard(types, button):
+    if types == 'Press':
+        keyboard_controller.press(button)
+    elif types == 'Release':
+        keyboard_controller.release(button)
+    else:
+        print(types, 'is not supported')
